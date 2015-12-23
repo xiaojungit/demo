c733d0653b89817f0c81f9e32537a5ebbc062d30
= <=>
2015-12-23 16:05:46 +0800

    test 1

--- untitled/src/main/java/com/test/dubbo/registry/serviceImpl/TestRegistryServiceImpl.java
+++ untitled/src/main/java/com/test/dubbo/registry/serviceImpl/TestRegistryServiceImpl.java
@@ -6,7 +6,7 @@ import com.test.dubbo.registry.service.TestRegistryService;
  * Created by Admin on 2015/12/22.
  */
 public class TestRegistryServiceImpl implements TestRegistryService {
-
+    //one
     public String hello(String name) {
         return "hello" + name;
     }
