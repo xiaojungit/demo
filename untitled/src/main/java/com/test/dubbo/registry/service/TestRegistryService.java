c863e2c0b76f39659843c103441b9cf74c079120
= <=>
2015-12-23 15:44:25 +0800

    Initial commit

---
+++ untitled/src/main/java/com/test/dubbo/registry/service/TestRegistryService.java
@@ -0,0 +1,8 @@
+package com.test.dubbo.registry.service;
+
+/**
+ * Created by Admin on 2015/12/22.
+ */
+public interface TestRegistryService {
+    public String hello(String name);
+}
---
+++ untitled/src/main/java/com/test/dubbo/registry/serviceImpl/TestRegistryServiceImpl.java
@@ -0,0 +1,15 @@
+package com.test.dubbo.registry.serviceImpl;
+
+import com.test.dubbo.registry.service.TestRegistryService;
+
+/**
+ * Created by Admin on 2015/12/22.
+ */
+public class TestRegistryServiceImpl implements TestRegistryService {
+
+    public String hello(String name) {
+        return "hello" + name;
+    }
+}
+
+
